6f1b0094f2bda2e1efd3e0cdb1635a917521b952
Emmanuel <edogbevi@gmail.com>
2014-04-14 03:12:21 +0000

    final commit

--- bootstrap.js
+++ bootstrap.js
@@ -3,8 +3,19 @@
  */
 
 
+var video = <"iframe width='560' height='315' src='http://www.youtube.com/embed/lYj0vFRf4gM'frameborder='0' allowfullscreen></iframe">;
 
 
-	
+ $(document).ready(function(){
+ 
+  setNav();
+ });
 
- }
+function setNav(){
+
+$(".NDC1").on("click", function(){
+
+$('#myModal').modal();
+});	
+
+}
